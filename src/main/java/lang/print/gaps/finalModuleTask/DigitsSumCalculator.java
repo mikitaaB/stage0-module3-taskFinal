ead690a535f24972c463eadaba6535d66353014a
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-25 03:23:00 +0300

    module 3. final

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        int remainingNumber = number;
+
+        while (remainingNumber > 0) {
+            int digit = remainingNumber % 10;
+            sum += digit;
+            remainingNumber /= 10;
+        }
+
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int aValue = number % 10;
+        int bValue = (number / 10) % 10;
+        int cValue = number / 100;
+        System.out.println((aValue * 100) + (bValue * 10) + cValue);
     }
 }
